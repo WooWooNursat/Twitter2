d132fa4e6558452e3fa9ec33648f7f31cd8c5ad6
Nursat <nursat@1fit.app>
2023-11-03 19:49:54 +0600

    Initial Commit

---
+++ Twitter2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter2
+//
+//  Created by Nursat Saduakhassov on 03.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
